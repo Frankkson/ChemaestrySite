412142f4a234c9b1bebaf46d4d8021edf41606df
Franklin <ffrancadesouza@gmail.com>
2023-09-18 14:30:48 -0300

    esqueleto do site do chemaestry

---
+++ index.js
@@ -0,0 +1,6 @@
+const redirect = () => {
+	window.location =
+		"https://drive.google.com/file/d/11-Xix3U8U5rUV8ntemsZj-dXnP3lbaMH/view?usp=drive_link";
+};
+
+document.getElementById("download").addEventListener("click", redirect);
